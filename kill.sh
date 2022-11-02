9d5371b6737615bf969087cda388d70d4f8e23e8
muranran <d1577707716@163.com>
2022-11-02 09:27:35 +0800

    dqn_opt_experimental

--- kill.sh
+++ kill.sh
@@ -1,7 +1,8 @@
-# shellcheck disable=SC2046
+#!/bin/bash
 kill -9 $(pgrep xt_main)
 kill -9 $(pgrep xt_explorer)
 kill -9 $(pgrep xt_broker)
 kill -9 $(pgrep plasma_store_se)
 kill -9 $(pgrep multi_trainer)
 kill -9 $(pgrep xt_compress)
+
